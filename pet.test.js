0c960ed5edeb8c9ed87e315ec9b7b15fc1e6d6a1
Jack Mills Davidson <jackmillsdavidson@gmail.com>
2023-01-01 16:25:48 +0000

    Update age and growUp function

--- pet.test.js
+++ pet.test.js
@@ -1,6 +1,18 @@
 const Pet = require("./pet");
 
 describe("constructor", () => {
+  it("increments the age by 1", () => {
+    const pet = new Pet("Fido");
+
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+  });
+  it("has a initial age of 0", () => {
+    const pet = new Pet("Fido");
+
+    expect(pet.age).toEqual(0);
+  });
   it("returns an object", () => {
     expect(new Pet("Fido")).toBeInstanceOf(Object);
   });
